52b6db58089012634a70a3b15956336d69fe0d3e
unknown <jupiter85838@yahoo.com>
2021-01-31 14:29:43 -0500

    invert tree

--- invert_tree/python_impl/invert_tree_test/test_display.py
+++ invert_tree/python_impl/invert_tree_test/test_display.py
@@ -12,13 +12,20 @@ def make_node():
     root.right.right = NewNode(5)
     return root
 
+@pytest.fixture
+def node_string(make_node):
+    return str(make_node)
 
+@pytest.mark.skip(reason="wrong way of testing")
 def test_compare(make_node):
-    #assert supply_data[0] == "NYC", "should pass"
     result = []
     expect = [1, 2, 3, 4, 5]
     result.append(print_tree(make_node))
     print(make_node)
     for index,ele in  enumerate(expect):
         assert ele ==result[index] , "should pass"
-    #assert result == expect, "should pass"
+
+def test_node_display(node_string,make_node):
+    print(make_node)
+    assert node_string==str(make_node),"should pass"
+    
